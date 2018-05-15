d1728c327b91da547387c338e370974a4b3e6c23
Qingyang Chen <qingyangc@google.com>
2018-05-15 18:56:20 -0400

    remove cruff

--- kube-up.sh
+++ kube-up.sh
@@ -10,17 +10,3 @@ kubectl run customers-service --expose --port=80 --image=gcr.io/qingyangc-sandbo
 
 kubectl run api-gateway --port=80 --image=gcr.io/qingyangc-sandbox/spring-petclinic:spring-petclinic-api-gateway --image-pull-policy=Always
 kubectl expose deploy api-gateway --type=LoadBalancer
-#
-#kubectl run vets-service --port=80 --image=gcr.io/qingyangc-sandbox/spring-petclinic:spring-petclinic-vets-service --image-pull-policy=Always
-#kubectl expose deploy vets-service --type=NodePort
-#
-#kubectl run visits-service --port=80 --image=gcr.io/qingyangc-sandbox/spring-petclinic:spring-petclinic-visits-service --image-pull-policy=Always
-#kubectl expose deploy visits-service --type=NodePort
-#
-#kubectl run customers-service --port=80 --image=gcr.io/qingyangc-sandbox/spring-petclinic:spring-petclinic-customers-service  --image-pull-policy=Always
-#kubectl expose deploy customers-service --type=NodePort
-#
-#kubectl run api-gateway --port=80 --image=gcr.io/qingyangc-sandbox/spring-petclinic:spring-petclinic-api-gateway --image-pull-policy=Always
-#kubectl expose deploy api-gateway --type=NodePort
-#
-#kubectl create -f kube-ingress.yaml
